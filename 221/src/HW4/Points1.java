5e72bb0fe2908c869119945f5fdced091da0a959
Jacob Manning <manningjc@g.cofc.edu>
2020-05-20 16:04:20 -0400

    Add comments and parentheses

--- 221/src/HW4/Points1.java
+++ 221/src/HW4/Points1.java
@@ -12,23 +12,28 @@ package HW4;
 
 public class Points1 {
 
+    // Instance variables
     private int xCoord;
     private int yCoord;
 
+    // Base constructor
     public Points1() {
         this.xCoord = 0;
         this.yCoord = 0;
     }
 
+    // User has a point object
     public Points1(Points1 point) {
+
+        // Point cannot be null, if null does nothing
         if(point != null) {
-            if(point.getX() >= 0 && point.getX() <= GridConstants.MAX_PANEL_WIDTH) {
+            if((point.getX() >= 0) && (point.getX() <= GridConstants.MAX_PANEL_WIDTH)) {
                 this.xCoord = point.getX();
             }
             else {
                 this.xCoord = 0;
             }
-            if(point.getY() >= 0 && point.getY() <= GridConstants.MAX_PANEL_HEIGHT) {
+            if((point.getY() >= 0) && (point.getY() <= GridConstants.MAX_PANEL_HEIGHT)) {
                 this.yCoord = point.getY();
             }
             else {
@@ -37,25 +42,27 @@ public class Points1 {
         }
     }
 
+    // User has 'x' and 'y' coordinates
     public Points1(int xPlane, int yPlane) {
-        if(xPlane >= 0 && xPlane <= GridConstants.MAX_PANEL_WIDTH) {
+        if((xPlane >= 0) && (xPlane <= GridConstants.MAX_PANEL_WIDTH)) {
             this.xCoord = xPlane;
         }
-        if(yPlane >= 0 && yPlane <= GridConstants.MAX_PANEL_HEIGHT) {
+        if((yPlane >= 0) && (yPlane <= GridConstants.MAX_PANEL_HEIGHT)) {
             this.yCoord = yPlane;
         }
     }
 
+    // Getter methods for 'xCoord' and 'yCoord'
     public int getX() {
         return xCoord;
     }
-
     public int getY() {
         return yCoord;
     }
 
+    // User creating a new 'xCoord', if value isn't within bounds value is zero
     public void setX(int xCoord) {
-        if(xCoord >= 0 && xCoord <= GridConstants.MAX_PANEL_WIDTH) {
+        if((xCoord >= 0) && (xCoord <= GridConstants.MAX_PANEL_WIDTH)) {
             this.xCoord = xCoord;
         }
         else {
@@ -63,8 +70,9 @@ public class Points1 {
         }
     }
 
+    // User creating a new 'yCoord', if value isn't within bounds value is zero
     public void setY(int yCoord) {
-        if(yCoord >= 0 && yCoord <= GridConstants.MAX_PANEL_HEIGHT) {
+        if((yCoord >= 0) && (yCoord <= GridConstants.MAX_PANEL_HEIGHT)) {
             this.yCoord = yCoord;
         }
         else {
@@ -72,10 +80,11 @@ public class Points1 {
         }
     }
 
-    public void setPoint(Points1 point) {
-        if(point != null) {
-            setX(point.getX());
-            setY(point.getY());
+    // User creating a new point
+    public void setPoint(Points1 p) {
+        if(p != null) {
+            setX(p.getX());
+            setY(p.getY());
         }
     }
 
--- 221/src/Tester.java
+++ 221/src/Tester.java
@@ -1,97 +1,87 @@
-/** Jacob Manning
- * CSCI 221, HW2
- *
- * I, Jacob Manning, certify that this is my individual work.
- *
- */
 
-//package HW2;
-
-import java.util.Scanner;
+import HW4.GridConstants;
+//import HW4.Points1;
 
 public class Tester {
 
-    // Method used for adding each individual number together
-    public int addingUp(int topNum, int botNum) {
-        int value1 = topNum - '0';
-        int value2 = botNum - '0';
-        int addedSum = value1 + value2;
-        return addedSum;
+    public class Points1 {
+
+        // Instance variables
+        private int xCoord;
+        private int yCoord;
+
+        // Base constructor
+        public Points1() {
+            this.xCoord = 0;
+            this.yCoord = 0;
+        }
+
+        // User has a point object
+        public Points1(HW4.Points1 point) {
+
+            // Point cannot be null, if null does nothing
+            if(point != null) {
+                if((point.getX() >= 0) && (point.getX() <= GridConstants.MAX_PANEL_WIDTH)) {
+                    this.xCoord = point.getX();
+                }
+                else {
+                    this.xCoord = 0;
+                }
+                if((point.getY() >= 0) && (point.getY() <= GridConstants.MAX_PANEL_HEIGHT)) {
+                    this.yCoord = point.getY();
+                }
+                else {
+                    this.yCoord = 0;
+                }
+            }
+        }
+
+        // User has 'x' and 'y' coordinates
+        public Points1(int xPlane, int yPlane) {
+            if((xPlane >= 0) && (xPlane <= GridConstants.MAX_PANEL_WIDTH)) {
+                this.xCoord = xPlane;
+            }
+            if((yPlane >= 0) && (yPlane <= GridConstants.MAX_PANEL_HEIGHT)) {
+                this.yCoord = yPlane;
+            }
+        }
+
+        // Getter methods for 'xCoord' and 'yCoord'
+        public int getX() {
+            return xCoord;
+        }
+        public int getY() {
+            return yCoord;
+        }
+
+        // User creating a new 'xCoord', if value isn't within bounds value is zero
+        public void setX(int xCoord) {
+            if((xCoord >= 0) && (xCoord <= GridConstants.MAX_PANEL_WIDTH)) {
+                this.xCoord = xCoord;
+            }
+            else {
+                this.xCoord = 0;
+            }
+        }
+
+        // User creating a new 'yCoord', if value isn't within bounds value is zero
+        public void setY(int yCoord) {
+            if((yCoord >= 0) && (yCoord <= GridConstants.MAX_PANEL_HEIGHT)) {
+                this.yCoord = yCoord;
+            }
+            else {
+                this.yCoord = 0;
+            }
+        }
+
+        // User creating a new point
+        public void setPoint(HW4.Points1 p) {
+            if(p != null) {
+                setX(p.getX());
+                setY(p.getY());
+            }
+        }
+
     }
 
-    public static void main(String[] args) {
-
-        String total = "";
-        int carryOver = 0;
-        int placeHolder = 1;
-
-        Tester columnTotal = new Tester();
-        Scanner request = new Scanner(System.in);
-
-        // Requesting input, of two string as numbers, from the user
-        System.out.println("Type your first number: ");
-        String firstInput = request.next();
-        System.out.println("Type your second number: ");
-        String secondInput = request.next();
-
-        request.close();
-
-        int strOneLength = firstInput.length();
-        int strTwoLength = secondInput.length();
-
-        // 2 for loops checking for letters
-        for (int i = 0; i < strOneLength; i++) {
-            if (!Character.isDigit(firstInput.charAt(i)))  {
-                System.out.println("That is not a number.");
-                return;
-            }
-        }
-        for (int i = 0; i < strTwoLength; i++) {
-            if (!Character.isDigit(secondInput.charAt(i)))  {
-                System.out.println("That is not a number.");
-                return;
-            }
-        }
-        // 2 if statements checking for the length of the string from the user
-        if (strOneLength > strTwoLength) {
-            // For loops used to append a '0' at the front of the string
-            // if one string is longer than the other
-            for (int k = strTwoLength; strOneLength > k; k++) {
-                secondInput = "0" + secondInput;
-                strTwoLength = secondInput.length();
-            }
-        }
-        if (strTwoLength > strOneLength) {
-            for (int j = strOneLength; strTwoLength > j; j++) {
-                firstInput = "0" + firstInput;
-                strOneLength = firstInput.length();
-            }
-        }
-        // Reads correctly---
-        System.out.println(firstInput + "  a " + secondInput);
-
-        // convert back to string and append to 'total' to stack numbers instead of using placeholder
-        while (strOneLength == strTwoLength) {
-            for (int j = strOneLength-1; j >= 0 ; j--) {
-                int doubleDigits = columnTotal.addingUp(firstInput.charAt(j), secondInput.charAt(j));
-                System.out.println(firstInput.charAt(j) + " b  " + secondInput.charAt(j));
-                if (carryOver == 1) {
-                    doubleDigits += 1;
-                    carryOver = 0;
-                    System.out.println(carryOver + " c  " + total);
-                }
-                // But what if it equals 10
-                // -- 10 doesn't register, spits out 0 because you subtract 10
-                else if (doubleDigits > 9) {
-                    carryOver++;
-                    doubleDigits -= 10;
-                    System.out.println(carryOver + " d  " + total);
-                }
-                total = doubleDigits + total;
-                placeHolder *= 10;
-            }
-            System.out.println(total);
-            break;
-        }
-    }
 }
